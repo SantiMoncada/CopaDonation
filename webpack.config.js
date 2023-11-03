80680a66ae8963a160d29f4a414c22d294b30c26
Santiago Moncada <santiago.m.a@hotmail.com>
2023-11-03 16:33:51 +0100

    added tailwind

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
 const path = require("path");
 
 module.exports = {
+  mode: "production",
   entry: "./js/index.js",
   output: {
-    path: path.resolve(__dirname, "public/scripts"),
+    path: path.resolve(__dirname, "public/dist"),
     filename: "index.js",
   },
-  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, "css"),
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+    ],
+  },
 };
